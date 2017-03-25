2ff757b72dc7f348bde59164fbbf24b6a19fd3f4
windsunrain <windsunrain@gmail.com>
2017-03-25 15:36:57 -0700

    The beginning

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, "dist"), // string
+    filename: "bundle.js", // string
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [path.resolve(__dirname, "src")],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"]
+            },
+          },
+          {
+            loader: "eslint-loader",
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          // apply multiple loaders and options
+          "htmllint-loader",
+          {
+            loader: "html-loader",
+            options: {
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "src")
+    ],
+    extensions: [".js", ".json", ".jsx", ".css"],
+
+    alias: {
+    },
+  },
+
+  devtool: "source-map", // enum
+
+  context: __dirname, // string (absolute path!)
+
+  target: "web", // enum
+
+  devServer: {
+    proxy: { // proxy URLs to backend development server
+      '/api': 'http://localhost:3000'
+    },
+    contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
+    compress: true, // enable gzip compression
+    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    https: false, // true for self-signed, object for cert authority
+    noInfo: true, // only errors & warns on hot reload
+    // ...
+  },
+}
