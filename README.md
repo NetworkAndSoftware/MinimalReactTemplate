9f2a26f201f570ca2e7cab7de93c80e35ecc0332
windsunrain <windsunrain@gmail.com>
2019-07-28 16:16:18 -0700

    Make yarn devserver work correctly

--- README.md
+++ README.md
@@ -14,8 +14,8 @@ to transpile and bundle.
 
 And then open public\index.html in a browser. If all went well, you should see a page that displays Hello World.
 
-You can also do
+During development it may be more convenient to run
 
 >yarn devserver
 
-This will start a local web server on port 8080, so you can then open <http://localhost:8080/> in your browser.
+This will start a local web server on port 8080. You can then open <http://localhost:8080/webpack-dev-server/> in your browser and whenever you make changes to any files webpack will rebuild the application and reload the web browser and display any error messages there.
--- webpack.config.js
+++ webpack.config.js
@@ -61,15 +61,12 @@ module.exports = {
   target: "web", // enum
 
   devServer: {
-    proxy: { // proxy URLs to backend development server
-      '/api': 'http://localhost:3000'
-    },
     contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
     compress: true, // enable gzip compression
     historyApiFallback: true, // true for index.html upon 404, object for multiple paths
     hot: false, // hot module replacement. Depends on HotModuleReplacementPlugin
     https: false, // true for self-signed, object for cert authority
-    noInfo: true, // only errors & warns on hot reload
-    // ...
+    noInfo: false,
+    inline: false
   },
 }
